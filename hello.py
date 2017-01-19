0df47efc5753071460da594f4c7df7108a0f3693
Daksh Sharma <dashatrix@gmail.com>
2017-01-18 21:52:23 -0800

    key val range for days of week

--- hello.py
+++ hello.py
@@ -1,31 +1,36 @@
 # This is a comment
 
 world = "Hello World!"
-print world
+print (world)
 print (world[0:6])
 print (world[6:12])
 print (5 * 8)
 
 PI = 3.14
-print PI
+print (PI)
 
 # list can hold multipe value types
 DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-print DAYS
-print DAYS[3:7]
+print (DAYS)
+print (DAYS[3:7])
 
 # number and day dictionary, Key : PairValue
 # starting at 0 to 6, Sunday to Saturday
 numAndDays = {}
 for i in range(len(DAYS)):
     numAndDays[i] = DAYS[i]
-print numAndDays
+    
+for day,val in numAndDays.items():
+    print day, ":", val
+# print numAndDays
 
 
 # Defining functions
 def isPalindrone(word):
-    word = word.replace(' ', '') #eliminate all spaces
-    word = word.lower() #make everything lowercase
+    # eliminate all spaces
+    word = word.replace(' ', '')
+    # make everything lowercase
+    word = word.lower()
     # python ternary return result(truthy) if condition else falsey
     return "Palindrone" if word == word[::-1] else "Not Palindrone"
     # if word == word[::-1]:
